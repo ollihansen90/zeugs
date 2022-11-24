0a732389e30e1a6beee7e0f101e597ac3cb752af
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2022-11-24 15:25:45 +0100

    Update ML_utils.py

--- ML_utils.py
+++ ML_utils.py
@@ -1,22 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-class LinearFunction():
-    def __init__(self, m=1, b=1):
+class lineareFunktion():
+    def __init__(self, m=1, b=-1):
         self.m = m
         self.b = b
-        
+
     def __call__(self, x):
         return self.m*x+self.b
 
     def __str__(self):
         if self.m==0:
             return str(self.b)
-        op = "+" if self.b>0 else "-"
+        op = "+" if self.b>=0 else "-"
         b = self.b
         if b==0:
             return f"{self.m}*x"
-        return f"{self.m}*x{op}{np.abs(b)}" 
-       
+        return f"{self.m}*x{op}{np.abs(b)}"
+
+    def copy(self):
+        return lineareFunktion(self.m, self.b)
+        
 class QuadraticFunction():
     def __init__(self, a=1, b=1, c=1):
         self.a = a
@@ -30,3 +34,6 @@ class QuadraticFunction():
         op1 = "+" if self.b>=0 else "-"
         op2 = "+" if self.c>=0 else "-"
         return f"{self.a}*x^2{op1}{np.abs(self.b)}*x{op2}{np.abs(self.c)}"
+        
+    def copy(self):
+        return QuadraticFunction(self.a, self.b, self.c)
