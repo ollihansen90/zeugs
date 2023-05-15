c8c6001f45df40908da54e657a84c7986f971764
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2023-05-15 13:54:10 +0200

    Update utils_bv.py

--- utils/utils_bv.py
+++ utils/utils_bv.py
@@ -6,14 +6,14 @@ def conv2d(img, kernel):
     out = np.zeros_like(img)
     for n in range(nn):
         im = img[:,:,n]
-        img_padded = pad(im)
+        img_padded = pad(im, rand=2*int(ki/2))
         for i in range(ki):
             for j in range(kj):
                 #print(img_padded[i:ii+i, j:jj+j].shape)
                 out[:,:,n] += img_padded[i:ii+i, j:jj+j]*kernel[i,j]
     return out
 
-def pad(img, mode="zeros"):
-    out = np.zeros([img.shape[0]+2, img.shape[1]+2])
-    out[1:-1,1:-1] = img
+def pad(img, mode="zeros", rand=2):
+    out = np.zeros([img.shape[0]+rand, img.shape[1]+rand])
+    out[int(rand/2):-int(rand/2),int(rand/2):-int(rand/2)] = img
     return out
